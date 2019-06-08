14eb442886014ab15324f8ca6e12881f8a08c690
AJ Guiles <ashiguiles@gmail.com>
2019-06-08 21:03:49 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -2,7 +2,7 @@
 
 def highest_student_gpa
   
-  SELECT Max(gpa) FROM students;
+  SELECT MAX(gpa) FROM students;
   
 end
 
@@ -14,7 +14,7 @@ end
 
 def average_student_gpa
   
-  SELECT MIN(gpa) FROM students;
+  SELECT AVG(gpa) FROM students;
   
 end
 
