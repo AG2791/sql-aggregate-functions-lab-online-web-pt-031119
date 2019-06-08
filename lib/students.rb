c8cb6372feace21a8868577b3d56699108c1222a
AJ Guiles <ashiguiles@gmail.com>
2019-06-08 21:07:03 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -2,25 +2,25 @@
 
 def highest_student_gpa
   
-  SELECT MAX(gpa) FROM students;
+  "SELECT MAX(gpa) FROM students;"
   
 end
 
 def lowest_student_gpa
   
-  SELECT MIN(gpa) FROM students;
+  "SELECT MIN(gpa) FROM students;"
   
 end
 
 def average_student_gpa
   
-  SELECT AVG(gpa) FROM students;
+  "SELECT AVG(gpa) FROM students;"
   
 end
 
 def total_tardies_for_all_students
   
-  SELECT SUM(tardies) FROM students;
+  "SELECT SUM(tardies) FROM students;"
   
 end
 
